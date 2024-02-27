4c336eab1ee8297a1e4923a96ef723f836bd46fa
JhonJSC182 <jhonjsc182@hotmail.com>
2024-02-27 14:29:42 -0800

    finish phase 2

--- phase-1-arrays.js
+++ phase-1-arrays.js
@@ -44,3 +44,63 @@
 
 // const arr = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
 // console.log(arr.transpose()) // => [[0,3,6], [1,4,7], [2,5,8]]
+
+
+
+//Phase 2
+
+//#4
+// const doubler = number => number * 2;
+// const arr = [1,2,3]
+
+// Array.prototype.myEach = function(callback){
+
+//     for (i = 0; i < this.length; i++) {
+//         this[i] = callback(this[i])
+//     }
+//     return this
+// }
+
+// console.log(arr.myEach(doubler))
+
+
+
+// #5
+// const doubler = number => number * 2;
+// const arr = [1,2,3]
+
+// Array.prototype.myMap = function(callback){
+//     let newArray = []
+//     for (i = 0; i < this.length; i++) {
+//         newArray.push(callback(this[i]))
+//     }
+//     return newArray
+// }
+
+// console.log(arr.myMap(doubler))
+
+
+// #6
+
+Array.prototype.myReduce = function(callback, initialValue) {
+    let accumulator;
+    let startIndex;
+    if (initialValue !== undefined) {
+        accumulator = initialValue;
+        startIndex = 0;
+    } else {
+        accumulator = this[0];
+        startIndex = 1;
+    }
+    for (i = startIndex; i < this.length; i ++) {
+        accumulator += this[i];
+    }
+
+    return accumulator
+}
+
+
+const arr = [1,2,3]
+const multiply = (accumulator, current) => accumulator * current
+console.log(arr.myReduce(multiply))
+
